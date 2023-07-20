3e128862be84981c92e76021c6dcae8c0c59ce23
Elam Vera Castro <elanvera@hotmail.com>
2023-07-19 19:36:44 -0500

    Salary projection

--- math_with_arrays_objects/analisis.js
+++ math_with_arrays_objects/analisis.js
@@ -15,4 +15,26 @@ function medianaPorPersona(nombrePersona){
     return medianaSalarios
 }
 
+function proyeccionPorPersona(nombrePersona){
+    const trabajos = encontrarPersona(nombrePersona).trabajos
 
+    let porcentajesCrecimiento = []
+
+    for (i = 1; i < trabajos.length; i++) {
+        const salarioActual = trabajos[i].salario
+
+        const salarioPasado = trabajos[i-1].salario
+
+        const crecimeinto = salarioActual - salarioPasado
+
+        const porcentajeCrecimiento = crecimeinto/salarioPasado
+
+        porcentajesCrecimiento.push(porcentajeCrecimiento)
+    }
+    console.log(porcentajesCrecimiento)
+    const medianaPorcentajeCrecimiento = Platzi_math.median(porcentajesCrecimiento)
+    const ultimoSalario = trabajos[trabajos.length -1].salario
+    const aumento = ultimoSalario*medianaPorcentajeCrecimiento
+    const nuevoSalario = aumento + ultimoSalario
+    return nuevoSalario
+}
