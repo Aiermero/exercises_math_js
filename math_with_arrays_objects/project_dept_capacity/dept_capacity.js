5eaae3e9de84c510c3d580b6b1e1fcbf8ac83355
Elam Vera Castro <elanvera@hotmail.com>
2023-08-02 14:03:44 -0500

    project finished

--- math_with_arrays_objects/project_dept_capacity/dept_capacity.js
+++ math_with_arrays_objects/project_dept_capacity/dept_capacity.js
@@ -12,12 +12,15 @@ const cuadroCalc = document.querySelector('.cuadro-calculo')
 const interestCalButton = document.querySelector('.calcular-deuda')
 const retunrBtn = document.querySelector('.return')
 const incomeAval = document.querySelector('.ingreso-disponible-num')
+const h2CalNewDept = document.querySelector('.calc-nueva-deuda')
+const h2CalIncome = document.querySelector('.cal-nuevo-ingreso')
 
 /*Calculation from input*/
 let netIncomeCal = 0
 let totalNewDeptCal = 0
 let totalInterestCal = 0
 let monthlyPay = 0
+const arrayConf = []
 
 /* Events */
 netIncomeButton.addEventListener('click', actualIncome)
@@ -33,6 +36,8 @@ function actualIncome(){
     incomeAval.textContent = netIncomeCal
     netDeptDiv.classList.toggle('inactive')
     netIncomeDiv.classList.toggle('inactive')
+    h2CalIncome.classList.toggle('inactive')
+    h2CalNewDept.classList.toggle('inactive')
 }
 
 function netDeptCalculator (){
@@ -43,20 +48,34 @@ function netDeptCalculator (){
     totalNewDeptCal = totalInterestCal + netNewDeptValue
     monthlyPay = (totalNewDeptCal/monthsValue).toFixed(2)
     const deptCapacity = (netIncomeCal * 30)/100
-
-    if ( monthlyPay > deptCapacity ) {
-        const parrafoConsejoNo =  document.createElement('p')
-        parrafoConsejoNo.classList.add('consejo-no')
-        parrafoConsejoNo.textContent = 'Tu deuda sería de ' + monthlyPay +' mensuales. No es recomendable endeudarte de esa manera debido a que es mayor que ' + deptCapacity + ', mas alto del 30% de tus ingresos netos.'
-        netDeptDiv.appendChild(parrafoConsejoNo)
-    }else {
-        const parrafoConsejoSi =  document.createElement('p')
-        parrafoConsejoSi.classList.add('consejo-si')
-        parrafoConsejoSi.textContent = 'Tu deuda sería de ' + monthlyPay +' mensuales. Es menor a' + deptCapacity + ', tu 30% de ingresos netos. Financieramente estas apto para adquirir tu deuda'
-        netDeptDiv.appendChild(parrafoConsejoSi)
+    if(arrayConf[0] == 1){
+        if(monthlyPay > deptCapacity){
+            let advise = document.querySelector('.advise')
+            advise.textContent = 'Tu deuda sería de ' + monthlyPay +' mensuales. No es recomendable endeudarte de esa manera debido a que tus pagos son mayores que ' + deptCapacity + ', mas alto del 30% de tus ingresos netos.'
+        } else{
+            let advise = document.querySelector('.advise')
+            advise.textContent = 'Tu deuda sería de ' + monthlyPay +' mensuales. Es menor a ' + deptCapacity + ', tu 30% de ingresos netos. Financieramente estas apto para adquirir tu deuda'
+        }
+    } else{
+        arrayConf.push(1)
+        if ( monthlyPay > deptCapacity ) {
+            const parrafoConsejo =  document.createElement('p')
+            parrafoConsejo.classList.add('advise')
+            parrafoConsejo.textContent = 'Tu deuda sería de ' + monthlyPay +' mensuales. No es recomendable endeudarte de esa manera debido a que es mayor que ' + deptCapacity + ', mas alto del 30% de tus ingresos netos.'
+            netDeptDiv.appendChild(parrafoConsejo)
+        }else {
+            const parrafoConsejo =  document.createElement('p')
+            parrafoConsejo.classList.add('advise')
+            parrafoConsejo.textContent = 'Tu deuda sería de ' + monthlyPay +' mensuales. Es menor a ' + deptCapacity + ', tu 30% de ingresos netos. Financieramente estas apto para adquirir tu deuda'
+            netDeptDiv.appendChild(parrafoConsejo)
+            console.log(arrayConf)
+        }
     }
+    
 }
 function returnButton(){
     netDeptDiv.classList.toggle('inactive')
     netIncomeDiv.classList.toggle('inactive')
+    h2CalIncome.classList.toggle('inactive')
+    h2CalNewDept.classList.toggle('inactive')
 }
